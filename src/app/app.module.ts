b21511c61ab1cf996be8027e44e6da785dc28bd8
Gino Lee <ginolee859@gmail.com>
2023-03-26 06:40:01 -0400

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
-import { ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
-import { TopBarComponent } from "./top-bar/top-bar.component";
-import { ProductListComponent } from "./product-list/product-list.component";
+import { CartComponent } from "./cart/cart.component";
 import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
 import { ProductDetailsComponent } from "./product-details/product-details.component";
-import { CartComponent } from "./cart/cart.component";
+import { ProductListComponent } from "./product-list/product-list.component";
 import { ShippingComponent } from "./shipping/shipping.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
 
 @NgModule({
   imports: [
